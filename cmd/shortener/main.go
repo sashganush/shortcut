00a5d22d0e03bc8a85c101c51a2f166721bf7181
Alexander Ganush <alexander.ganush@exness.com>
2022-02-12 20:39:28 +0200

    Incriment1 create basic service

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,75 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"strings"
+	"time"
+)
+
+const SHORTURLLEN = 8
+
+var allRedirects = map[string]string{
+	"4444": "https://google.com/abcd",
+	"1111": "https://ya.ru/1",
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func PostRequestHandler(w http.ResponseWriter, r *http.Request) (s int) {
+
+	_, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return http.StatusInternalServerError
+	}
+
+	n := RandStringRunes(SHORTURLLEN)
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "https://tinyurl.com/%s\n", n)
+	return http.StatusCreated
+}
+
+func GetRequestHandler(w http.ResponseWriter, r *http.Request) (s int) {
+
+	if s, exists := allRedirects[strings.TrimPrefix(r.URL.Path, "/")]; exists {
+		http.Redirect(w, r, s, http.StatusTemporaryRedirect)
+		return http.StatusTemporaryRedirect
+	}
+	http.Error(w, "Unknown redirect", http.StatusBadRequest)
+	return http.StatusBadRequest
+}
+
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+
+	var s int
+
+	switch r.Method {
+	case "POST":
+		s = PostRequestHandler(w, r)
+	default:
+		s = GetRequestHandler(w, r)
+	}
+
+	fmt.Println(time.Now().Format(time.RFC3339), s, r.URL.Path)
+}
+
+func main() {
+
+	http.HandleFunc("/", MainHandler)
+	http.ListenAndServe(":8080", nil)
+}
