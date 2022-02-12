942cee88a6afe848fe3d1849cb3f279e172c8fb5
Alexander Ganush <alexander.ganush@exness.com>
2022-02-12 21:27:18 +0200

    Incriment1 fix http code for redirect request

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -14,6 +14,9 @@ const SHORTURLLEN = 8
 var allRedirects = map[string]string{
 	"4444": "https://google.com/abcd",
 	"1111": "https://ya.ru/1",
+	"kyQFSSqy": "http://t9xiost0mawj62.ru",
+	"ytOkkmjo": "http://y4qcfp8ur.yandex",
+
 }
 
 func init() {
