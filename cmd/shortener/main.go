50a7058e015bdf82eb769d2dcf41c7ff3a06bdf5
Alexander Ganush <alexander.ganush@exness.com>
2022-05-16 19:38:47 +0300

    Incremet7 add command line support

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,9 +7,11 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/sashganush/shortcut/internal/config"
 	handlers "github.com/sashganush/shortcut/internal/handlers"
+	"github.com/spf13/cobra"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 )
 
 func NewRouter() chi.Router {
@@ -32,21 +34,70 @@ func NewRouter() chi.Router {
 	return r
 }
 
+var (
+	cfgFile string
+
+	rootCmd = &cobra.Command{
+		Use:   "shortener",
+		Short: "Shortener Applications",
+		Long:  `Shortenter service create a simple redirect service for long urls.`,
+	}
+)
+
+func Execute() error {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	return nil
+}
+
+var flagServerAddress string
+var flagServerPort string
+var flagBaseUrl string
+var flagFileStoragePath string
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&flagServerAddress, "serveraddress","a", "", "service address the same from SERVER_ADDRESS")
+	rootCmd.PersistentFlags().StringVarP(&flagServerPort, "serverport","p", "", "service port the same from SERVER_PORT")
+	rootCmd.PersistentFlags().StringVarP(&flagBaseUrl, "baseurl","b", "", "base url the same like BASE_URL env")
+	rootCmd.PersistentFlags().StringVarP(&flagFileStoragePath, "filestoragepath","f", "", "file storage path the same like FILE_STORAGE_PATH env")
+}
 
 func main() {
 
 	if err := env.Parse(&config.Cfg); err != nil {
 		fmt.Println("failed:", err)
+		panic(err)
 	}
 
-	fmt.Printf("Start at %s%s\n", config.Cfg.ServerAddress,config.Cfg.ServerPort)
-	fmt.Printf("Start with base url %s\n", config.Cfg.BaseUrl)
-
-	u, err := url.Parse(config.Cfg.BaseUrl)
+	_, err := url.Parse(config.Cfg.BaseUrl)
 	if err != nil {
 		panic(err)
 	}
 
+	Execute()
+
+	if flagServerAddress != "" {
+		config.Cfg.ServerAddress = flagServerAddress
+	}
+
+	if flagBaseUrl != "" {
+		config.Cfg.BaseUrl = flagBaseUrl
+	}
+
+	if flagFileStoragePath != "" {
+		config.Cfg.FileStoragePath = flagFileStoragePath
+	}
+
+	fmt.Printf("Flag Server Address at %s\n", flagServerAddress)
+	fmt.Printf("Flag base_url at %s\n", flagBaseUrl)
+	fmt.Printf("Flag filestoragepath at %s\n", flagFileStoragePath)
+
+	fmt.Printf("Cfg Server Address at %s\n", config.Cfg.ServerAddress)
+	fmt.Printf("Cfg base_url at %s\n", config.Cfg.BaseUrl)
+	fmt.Printf("Cfg filestoragepath at %s\n", config.Cfg.FileStoragePath)
+
 	r := NewRouter()
-	log.Fatal(http.ListenAndServe(u.Host, r), nil)
+	log.Fatal(http.ListenAndServe(config.Cfg.ServerAddress, r), nil)
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,9 +2,9 @@ package config
 
 type Config struct {
 	ServerSchema    string `env:"SERVER_SCHEMA" envDefault:"http://"`
-	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1"`
+	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"0.0.0.0:8080"`
 	ServerPort      string `env:"SERVER_PORT" envDefault:":8080"`
-	BaseUrl         string `env:"BASE_URL" envDefault:"http://127.0.0.1:8080"`
+	BaseUrl         string `env:"BASE_URL" envDefault:"http://0.0.0.0:8080"`
 	BaseUri         string `env:"BASE_URI" envDefault:"/"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"/tmp/persistent"`
 }
